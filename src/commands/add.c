033aa2c9d6b2551d50a5775cf3659a370068e9e1
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-15 20:37:57 +0330

    working on add command module.

---
+++ src/commands/add.c
@@ -0,0 +1,12 @@
+#include "janus.h"
+
+void add(char *file) {
+  FILE *index = fopen(INDEX_FILE, "a");
+  if(!index) {
+    perror("Failed to open index file");
+    return;
+  }
+  fprintf(index, "%s\n", file);
+  fclose(index);
+  printf("Added '%s' to the staging area.\n", file);
+}
