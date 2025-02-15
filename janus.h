136b76eb25f01b5a19bbebb3e4900998e209741f
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-15 19:49:44 +0330

    project structure created and i worked on header file.

---
+++ janus.h
@@ -0,0 +1,22 @@
+#ifndef VCS_H
+#define VCS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define VCS_DIR ".janus"
+#define OBJECTS_DIR ".janus/objects"
+#define INDEX_FILE ".janus/index"
+#define HEAD_FILE ".janus/HEAD"
+#define LOGS_FILE ".janus/logs"
+
+// command funcs (this part will be increased in the future)
+void init();
+void add(const char *file);
+void commit(const char *message);
+void log();
+void status();
+
+#endif
