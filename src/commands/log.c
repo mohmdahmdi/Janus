e74686a73b8e7b893a029a251702bd03bedb9e7d
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-16 12:22:55 +0330

    working on log command module.

---
+++ src/commands/log.c
@@ -0,0 +1,14 @@
+#include "janus.h"
+
+void jlog() {
+  FILE *logs = fopen(LOGS_FILE, "r");
+  if (!logs) {
+      perror("Failed to open logs file");
+      return;
+  }
+  char ch;
+  while ((ch = fgetc(logs)) != EOF) {
+      putchar(ch);
+  }
+  fclose(logs);
+}
